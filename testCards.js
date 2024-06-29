ee003856158309ca602be07bef984b71b06960ac
migueldevacv <mikeangel7879@gmail.coom>
2024-06-28 18:23:23 -0600

    [UPD] solve the problem

--- testCards.js
+++ testCards.js
@@ -2,12 +2,14 @@ const numberCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const cardTypes = ["trebol", "spades", "diamond", "clubs"];
 
 function getAllOrderedDecks(allCards = 1000) {
-  console.time();
-
+  // console.time();
 
   //here first I create the dictionary to get the latest deck of the card
   const originalDeck = cardTypes.reduce((a, b) => {
-    return { ...a, ...numberCards.reduce((na, nb) => ({...na, [`${nb} - ${b}`]: 0}), {}) };
+    return {
+      ...a,
+      ...numberCards.reduce((na, nb) => ({ ...na, [`${nb} - ${b}`]: 0 }), {})
+    };
   }, {});
 
   const arrayCards = new Array(allCards).fill(0).map(card => {
@@ -20,8 +22,22 @@ function getAllOrderedDecks(allCards = 1000) {
     return newCard;
   });
 
-  //   const newArrayCards = Array.fill(allCards);
+  const arrayOfAllDecks = [];
+
+  console.time()
+  arrayCards.forEach(card => {
+    //here I take the latest index of the card and add the array with the new card and add 1 to the index because the card is on a deck
+    arrayOfAllDecks[originalDeck[card]] = [
+      ...(arrayOfAllDecks[originalDeck[card]] || []),
+      card
+    ];
+
+    originalDeck[card]++;
+  });
   console.timeEnd();
+
+  console.log(arrayOfAllDecks);
+  //   const newArrayCards = Array.fill(allCards);
 }
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
